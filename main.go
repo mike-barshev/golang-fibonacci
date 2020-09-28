92c95cde004c2f550bb998996abd19809cfd8669
mike-barshev <mike-barshev@users.noreply.github.com>
2020-09-29 01:09:56 +0300

    improved algorithm

--- main.go
+++ main.go
@@ -2,19 +2,10 @@ package main
 
 import "sync"
 
-func unaryExpression(x *int, wg *sync.WaitGroup) {
-	*x++
-	defer wg.Done()
-}
-
-// fibonacci function. Renews 'n-1' and 'n' elements
-func fibonacci(x, y *uint64, wg *sync.WaitGroup) {
-	*y = *x + *y
-	*x = *y - *x
-	defer wg.Done()
-}
-
-func main() {
+func fibonacci(n int) uint64 {
+	if n == 1 || n == 2 {
+		return 1
+	}
 	wg := new(sync.WaitGroup)
 	x := new(uint64)
 	y := new(uint64)
@@ -22,14 +13,27 @@ func main() {
 	*x = 1
 	*y = 1
 	*i = 1
-
-	n := 50 // fibonacci N number
-
 	for *i < n {
 		wg.Add(2)
+		go fibonacciRenews(x, y, wg)
 		go unaryExpression(i, wg)
-		go fibonacci(x, y, wg)
 		wg.Wait()
 	}
-	println(*x)
+	return *x
+}
+
+func unaryExpression(x *int, wg *sync.WaitGroup) {
+	*x++
+	defer wg.Done()
+}
+
+func fibonacciRenews(x, y *uint64, wg *sync.WaitGroup) {
+	*y = *x + *y
+	*x = *y - *x
+	defer wg.Done()
+}
+
+func main() {
+	n := 50 // fibonacci N number
+	println(fibonacci(n))
 }

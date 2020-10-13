cc5f85a105c4caf30ff938c9f9747693829aa7cd
mike-barshev <mike-barshev@users.noreply.github.com>
2020-10-13 21:48:44 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,39 +1,22 @@
 package main
 
-import "sync"
+import "fmt"
 
-func fibonacci(n int) uint64 {
-	if n == 1 || n == 2 {
-		return 1
-	}
-	wg := new(sync.WaitGroup)
-	x := new(uint64)
-	y := new(uint64)
-	i := new(int)
-	*x = 1
-	*y = 1
-	*i = 1
-	for *i < n {
-		wg.Add(2)
-		go fibonacciRenews(x, y, wg)
-		go unaryExpression(i, wg)
-		wg.Wait()
-	}
-	return *x
-}
+func fib() chan uint64 {
+	c := make(chan uint64)
+	var i, j uint64
+	go func() {
+		for i, j = 0, 1; ; i, j = i+j, i {
+			c <- i
+		}
+	}()
 
-func unaryExpression(x *int, wg *sync.WaitGroup) {
-	*x++
-	defer wg.Done()
-}
-
-func fibonacciRenews(x, y *uint64, wg *sync.WaitGroup) {
-	*y = *x + *y
-	*x = *y - *x
-	defer wg.Done()
+	return c
 }
 
 func main() {
-	n := 50 // fibonacci N number
-	println(fibonacci(n))
+	c := fib()
+	for n := 0; n < 1000; n++ {
+		fmt.Println(<-c)
+	}
 }
